83231c7822c510f744015983ea51697bc6b6e5fc
stursdev <stursdev@gmail.com>
2020-05-08 17:45:46 -0400

    Set Up for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
     msgTwo.textContent = ''
 
     // Fetch forecast data from weather-stack
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error){
                 msgOne.textContent = data.error
